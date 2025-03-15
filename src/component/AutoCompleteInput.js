811f89eca1d7ede9beb47055da216af663e843ee
kelvis1 <wive.rod20@gmail.com>
2025-03-15 16:17:33 -0300

    feat: adição de css com matireal UI

--- src/component/AutoCompleteInput.js
+++ src/component/AutoCompleteInput.js
@@ -1,75 +1,119 @@
-'use client';
+"use client";
 
-import React, { useEffect, useState } from 'react';
-import dynamic from 'next/dynamic';
+import React, { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 
-
-
-const GoogleMapComponent = dynamic(() => import("./GoogleMapComponent"), { ssr: false });
-const Directions = dynamic(() => import('./Directions'), { ssr: false });
+const GoogleMapComponent = dynamic(() => import("./GoogleMapComponent"), {
+  ssr: false,
+});
+const Directions = dynamic(() => import("./Directions"), { ssr: false });
 
 const AutoCompleteInput = () => {
+  const [origin, setOrigin] = useState("");
+  const [originlatilong, setoriginlatilong] = useState(null);
+  const [destinetionlatilong, setdestinationlatilong] = useState(null);
+  const [destination, setDestination] = useState("");
+  const [showDirections, setShowDirections] = useState(false);
 
-    const [origin, setOrigin] = useState('');
-    const [originlatilong, setoriginlatilong] = useState(null);
-    const [destinetionlatilong, setdestinationlatilong] = useState(null);
-    const [destination, setDestination] = useState('');
-    const [showDirections, setShowDirections] = useState(false);
+  const handleButtonClick = () => {
+    if (!origin || !destination) {
+      alert("Por favor, insira origem e destino.");
+      return;
+    }
+    setShowDirections(true);
+  };
 
-
-    const handleButtonClick = () => {
-        if (!origin || !destination) {
-            alert('Por favor, insira origem e destino.');
-            return;
-        }
-        setShowDirections(true);
+  useEffect(() => {
+    const loadScript = (url, callback) => {
+      const script = document.createElement("script");
+      script.src = url;
+      script.async = true;
+      script.defer = true;
+      script.onload = callback;
+      document.head.appendChild(script);
     };
 
-    useEffect(() => {
-        const loadScript = (url, callback) => {
-            const script = document.createElement('script');
-            script.src = url;
-            script.async = true;
-            script.defer = true;
-            script.onload = callback;
-            document.head.appendChild(script);
-        };
-
-        loadScript(
-            `https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`,
-            () => {
-                if (window.google) {
-                    const originAutocomplete = new google.maps.places.Autocomplete(document.getElementById('origin'), { types: ['geocode'] });
-                    originAutocomplete.addListener('place_changed', () => {
-                        const place = originAutocomplete.getPlace();
-                        if (place.geometry) {
-                            setOrigin(place.formatted_address);
-                        }
-                    });
-
-                    const destinationAutocomplete = new google.maps.places.Autocomplete(document.getElementById('destination'), { types: ['geocode'] });
-                    destinationAutocomplete.addListener('place_changed', () => {
-                        const place = destinationAutocomplete.getPlace();
-                        if (place.geometry) {
-                            setDestination(place.formatted_address);
-                        }
-                    });
-                }
+    loadScript(
+      `https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`,
+      () => {
+        if (window.google) {
+          const originAutocomplete = new google.maps.places.Autocomplete(
+            document.getElementById("origin"),
+            { types: ["geocode"] }
+          );
+          originAutocomplete.addListener("place_changed", () => {
+            const place = originAutocomplete.getPlace();
+            if (place.geometry) {
+              setOrigin(place.formatted_address);
             }
-        );
-    }, []);
+          });
 
-    return (
-        <div>
-            <GoogleMapComponent origin={originlatilong} destination={destinetionlatilong} />
-
-            <input id="origin" type="text" placeholder="Digite o ponto de partida" value={origin} onChange={(e) => setOrigin(e.target.value)} />
-            <input id="destination" type="text" placeholder="Digite o destino" value={destination} onChange={(e) => setDestination(e.target.value)} />
-            <button onClick={handleButtonClick}>Pesquisar</button>
-
-            {showDirections && <Directions origin={origin} destination={destination} setoriginlatilong={setoriginlatilong} setdestinationlatilong={setdestinationlatilong} />}
-        </div>
+          const destinationAutocomplete = new google.maps.places.Autocomplete(
+            document.getElementById("destination"),
+            { types: ["geocode"] }
+          );
+          destinationAutocomplete.addListener("place_changed", () => {
+            const place = destinationAutocomplete.getPlace();
+            if (place.geometry) {
+              setDestination(place.formatted_address);
+            }
+          });
+        }
+      }
     );
+  }, []);
+
+  return (
+    <div className="mapacontainer">
+      <div className="textocontainer">
+        <div className="origemtextfield">
+          <TextField
+            id="origin"
+            type="text"
+            placeholder="Digite o ponto de partida"
+            label="Origem"
+            variant="filled"
+            fullWidth
+            value={origin}
+            onChange={(e) => setOrigin(e.target.value)}
+          />
+        </div>
+        <div className="destinotextfield">
+          <TextField
+            id="destination"
+            type="text"
+            placeholder="Digite o destino"
+            label="Destino"
+            variant="filled"
+            fullWidth
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+          />
+        </div>
+        <div className="buttonpesquisar">
+          <Button fullWidth variant="contained" onClick={handleButtonClick}>
+            Pesquisar
+          </Button>
+        </div>
+        {showDirections && (
+          <Directions
+            origin={origin}
+            destination={destination}
+            setoriginlatilong={setoriginlatilong}
+            setdestinationlatilong={setdestinationlatilong}
+          />
+        )}
+      </div>
+      <div className="googlemapacontainer">
+        <GoogleMapComponent
+          origin={originlatilong}
+          destination={destinetionlatilong}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AutoCompleteInput;
--- src/component/GoogleMapComponent.js
+++ src/component/GoogleMapComponent.js
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 
 const containerStyle = {
   width: "100%",
-  height: "500px",
+  height: "100vh",
 };
 
 export default function GoogleMapComponent({ origin, destination }) {
